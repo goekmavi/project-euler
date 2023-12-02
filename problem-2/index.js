567f489e965aee74994aa38a61689348d2fcc3d8
goekmavi <goekmavidev@gmail.com>
2023-12-02 01:49:54 +0100

    Add solution for problem 2

---
+++ problem-2/index.js
@@ -0,0 +1,18 @@
+const limit = 4_000_000;
+
+let sum = 0;
+let current = 2;
+let previous = 1;
+
+while (current < limit) {
+    if (current % 2 === 0) {
+        sum += current;
+    }
+
+    const tempPrevious = previous;
+
+    previous = current;
+    current += tempPrevious;
+}
+
+console.log('Result;', sum);
